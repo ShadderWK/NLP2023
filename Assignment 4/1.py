e17dfbb7ea145056e160697ffd04f9770e04cfe7
Pirch <pirch999@gmail.com>
2023-05-09 15:13:15 +0700

    Fix Assignment 4

--- Assignment 4/1.py
+++ Assignment 4/1.py
@@ -1,47 +1,34 @@
 import nltk
 from nltk.tokenize import word_tokenize
-from nltk.tag import pos_tag
-from nltk.chunk import ne_chunk
-from nltk.chunk.util import conlltags2tree, tree2conlltags
+from nltk import pos_tag, ne_chunk
+from nltk.chunk import conlltags2tree, tree2conlltags
 from termcolor import colored
 
-# Sample paragraph
-with open('paragraph.txt','r') as file :
-    paragraph = file.read()
+paragraph = """At least 13 Palestinians, including three commanders of the militant group Islamic Jihad, have been killed in 
+Israeli air strikes on the Gaza Strip.
+A Palestinian health official said six women and four children were among the dead. Another 20 people were injured.
+Israel said it had launched an operation targeting militants who posed an imminent threat to its citizens.
+Islamic Jihad has vowed revenge and Gaza-based militants are expected to respond with rocket fire into Israel.
+Correspondents say one significant factor will be the extent to which Hamas, which controls the Strip, joins in.
+Israel officials are said to be preparing for days of fighting.
+The strikes were the deadliest since three days of fighting between Israel and Islamic Jihad last August."""
 
-# Tokenize paragraph into words
 words = word_tokenize(paragraph)
-
-# Perform Part-of-Speech tagging on words
 pos_tags = pos_tag(words)
-
-# Perform Named Entity Recognition on tagged words
 ne_tags = ne_chunk(pos_tags)
-
-# Convert the tags to IOB format
 iob_tags = tree2conlltags(ne_tags)
-
-# Convert the IOB format tags to a tree
 ne_tree = conlltags2tree(iob_tags)
 
-# Loop through words and tags and print with colored output
 print("POS: ")
 for word, tag in pos_tags:
     if 'NN' in tag:
         print(colored(word, 'red'), end=' ')
-    elif 'PERSON' in tag:
-        print(colored(word, 'red'), end=' ')
-    elif 'GPE' in tag or 'LOC' in tag:
-        print(colored(word, 'blue'), end=' ')
-    elif 'ORG' in tag:
-        print(colored(word, 'green'), end=' ')
     elif 'VB' in tag:
         print(colored(word, 'green'), end=' ')
     else:
         print(word, end=' ')
 print('\n')
 
-# Loop through named entities and print with colored output
 print("NER: ")
 for word, tag, iob in iob_tags:
     if iob == 'B-PERSON':
@@ -50,8 +37,7 @@ for word, tag, iob in iob_tags:
         print(colored(word, 'green'), end=' ')
     elif iob == 'B-ORG':
         print(colored(word, 'blue'), end=' ')
-    elif iob == 'B-VP':
-        print(colored(word, 'magenta'), end=' ')
     else:
         print(word, end=' ')
 print('\n')
+
